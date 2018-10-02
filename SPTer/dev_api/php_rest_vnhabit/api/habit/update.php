16442cc0bf22e59af3676732c454de512e1e94b1
dat <tvtd995a3@gmail.com>
2018-10-02 19:45:18 +0700

    update api

--- SPTer/dev_api/php_rest_vnhabit/api/habit/update.php
+++ SPTer/dev_api/php_rest_vnhabit/api/habit/update.php
@@ -22,14 +22,19 @@ $data = json_decode(file_get_contents("php://input"));
 // Set ID to update
 $habit->habit_id = $data->habit_id;
 
-$user->username = $data->username;
-$user->password = $data->password;
-$user->email = $data->email;
-$user->date_of_birth = $data->date_of_birth;
-$user->gender = $data->gender;
-$user->user_icon = $data->user_icon;
-$user->avatar = $data->avatar;
-$user->user_description = $data->user_description;
+$habit->user_id = $data->user_id;
+$habit->catagory_id = $data->catagory_id;
+$habit->schedule_id = $data->schedule_id;
+$habit->goal_id = $data->goal_id;
+$habit->habit_name = $data->habit_name;
+$habit->habit_type = $data->habit_type;
+$habit->unit = $data->unit;
+$habit->count_type = $data->count_type;
+$habit->start_date = $data->start_date;
+$habit->end_date = $data->end_date;
+$habit->created_date = $data->created_date;
+$habit->habit_icon = $data->habit_icon;
+$habit->habit_description = $data->habit_description;
 
 if ($habit->update()) {
     echo json_encode(
