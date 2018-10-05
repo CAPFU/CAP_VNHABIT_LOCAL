6689f84b36a99105ab26a6890a4eb94ef67edb8d
dat <tvtd995a3@gmail.com>
2018-10-05 09:04:49 +0700

    update

--- SPTer/dev_android/HabitTracker/app/src/main/java/habit/tracker/habittracker/MenuActivity.java
+++ SPTer/dev_android/HabitTracker/app/src/main/java/habit/tracker/habittracker/MenuActivity.java
@@ -3,8 +3,10 @@ package habit.tracker.habittracker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,7 +21,7 @@ public class MenuActivity extends AppCompatActivity implements MenuRecyclerViewA
         setContentView(R.layout.activity_menu);
 
         List<MenuItem> data = new ArrayList<>();
-        MenuItem item = new MenuItem("Personal", "12", R.drawable.ps);
+        MenuItem item = new MenuItem("PERSONAL", "12", R.drawable.ps);
         data.add(item);
         item = new MenuItem("WORK", "12", R.drawable.work);
         data.add(item);
@@ -29,12 +31,11 @@ public class MenuActivity extends AppCompatActivity implements MenuRecyclerViewA
         data.add(item);
         item = new MenuItem("PRIVATE", "12", R.drawable.privateic);
         data.add(item);
-        item = new MenuItem("ADD NEW", "", R.drawable.ps);
+        item = new MenuItem("ADD NEW", "", R.drawable.add);
         data.add(item);
 
         RecyclerView recyclerView = findViewById(R.id.rvMenu);
-        int numberOfColumns = 2;
-        recyclerView.setLayoutManager(new GridLayoutManager(this, numberOfColumns));
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
         adapter = new MenuRecyclerViewAdapter(this, data);
         adapter.setClickListener(this);
         recyclerView.setAdapter(adapter);
@@ -42,6 +43,6 @@ public class MenuActivity extends AppCompatActivity implements MenuRecyclerViewA
 
     @Override
     public void onItemClick(View view, int position) {
-
+        Toast.makeText(this, "p: " + position, Toast.LENGTH_SHORT).show();
     }
 }
