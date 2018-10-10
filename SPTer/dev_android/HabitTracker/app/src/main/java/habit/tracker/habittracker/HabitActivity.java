52a9e2e2cdbcc54181b089e65aad4ced9546ac99
dat <tvtd995a3@gmail.com>
2018-10-11 06:27:53 +0700

    update

--- SPTer/dev_android/HabitTracker/app/src/main/java/habit/tracker/habittracker/HabitActivity.java
+++ SPTer/dev_android/HabitTracker/app/src/main/java/habit/tracker/habittracker/HabitActivity.java
@@ -6,96 +6,131 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class HabitActivity extends AppCompatActivity implements View.OnClickListener {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
-    View btnBack;
-
-    Button cur1;
-    Button cur2;
+public class HabitActivity extends AppCompatActivity {
 
     // group 1
+    @BindView(R.id.g1_btn_build)
     Button btng1Build;
+    @BindView(R.id.g1_btn_quit)
     Button btng1Quit;
-    int g1Sel = 0;
+    int buildType = 0;
 
     // group 2
-    Button btng2Daily;
-    Button btng2Weekly;
-    Button btng2Monthly;
-    Button btng2Yearly;
-    int g2Sel = 0;
+    Button btnWatchMode;
+    @BindView(R.id.g2_btn_daily)
+    Button btnDaily;
+    @BindView(R.id.g2_btn_weekly)
+    Button btnWeekly;
+    @BindView(R.id.g2_btn_monthly)
+    Button btnMonthly;
+    @BindView(R.id.g2_btn_yearly)
+    Button btnYearly;
+    int watchMode = 0;
+
+    @BindView(R.id.tv_count_unit)
+    TextView tvCountUnit;
+    @BindView(R.id.ck_type_check)
+    View ckTypeCheck;
+    @BindView(R.id.ck_type_count)
+    View ckTypeCount;
+    @BindView(R.id.g3_ck_check)
+    ImageView imgTypeCheck;
+    @BindView(R.id.g3_ck_count)
+    ImageView imgTypeCount;
+    @BindView(R.id.edit_count)
+    EditText editCount;
+    int typeCount = 0;
+
+    @BindView(R.id.btn_unit_no)
+    Button btnNoUnit;
+    @BindView(R.id.btn_unit_set)
+    Button btnSetUnit;
+    String countUnit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_habit);
+        ButterKnife.bind(this);
 
-        btnBack = findViewById(R.id.btn_back);
-        btnBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                HabitActivity.this.finish();
-            }
-        });
-
-        btng1Build = findViewById(R.id.g1_btn_build);
-        btng1Build.setOnClickListener(this);
-        btng1Quit = findViewById(R.id.g1_btn_quit);
-        btng1Quit.setOnClickListener(this);
-        cur1 = btng1Build;
-
-        btng2Daily = findViewById(R.id.g2_btn_daily);
-        btng2Daily.setOnClickListener(this);
-        btng2Weekly = findViewById(R.id.g2_btn_weekly);
-        btng2Weekly.setOnClickListener(this);
-        btng2Monthly = findViewById(R.id.g2_btn_monthly);
-        btng2Monthly.setOnClickListener(this);
-        btng2Yearly = findViewById(R.id.g2_btn_yearly);
-        btng2Yearly.setOnClickListener(this);
-        cur2 = btng2Daily;
+        btnWatchMode = btnDaily;
     }
 
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
+    @OnClick({R.id.g1_btn_build, R.id.g1_btn_quit})
+    public void setBuildType(View v) {
+        switch (v.getId()) {
             case R.id.g1_btn_build:
-                setWhiteBg(cur1);
+                setWhiteBg(btng1Quit);
                 setGreenBg(btng1Build);
-                cur1 = btng1Build;
-                g1Sel = 0;
+                buildType = 0;
                 break;
             case R.id.g1_btn_quit:
-                setWhiteBg(cur1);
+                setWhiteBg(btng1Build);
                 setGreenBg(btng1Quit);
-                cur1 = btng1Quit;
-                g1Sel = 1;
-                break;
-            case R.id.g2_btn_daily:
-                setWhiteBg(cur2);
-                setGreenBg(btng2Daily);
-                cur2 = btng2Daily;
-                g2Sel = 0;
-                break;
-            case R.id.g2_btn_weekly:
-                setWhiteBg(cur2);
-                setGreenBg(btng2Weekly);
-                cur2 = btng2Weekly;
-                g2Sel = 1;
-                break;
-            case R.id.g2_btn_monthly:
-                setWhiteBg(cur2);
-                setGreenBg(btng2Monthly);
-                cur2 = btng2Monthly;
-                g2Sel = 2;
-                break;
-            case R.id.g2_btn_yearly:
-                setWhiteBg(cur2);
-                setGreenBg(btng2Yearly);
-                cur2 = btng2Yearly;
-                g2Sel = 3;
                 break;
         }
+    }
+
+    @OnClick({R.id.g2_btn_daily, R.id.g2_btn_weekly, R.id.g2_btn_monthly, R.id.g2_btn_yearly})
+    public void setWatchMode(View view) {
+        switch (view.getId()) {
+            case R.id.g2_btn_daily:
+                setWhiteBg(btnWatchMode);
+                setGreenBg(btnDaily);
+                tvCountUnit.setText("trong một ngày");
+                btnWatchMode = btnDaily;
+                watchMode = 0;
+                break;
+            case R.id.g2_btn_weekly:
+                setWhiteBg(btnWatchMode);
+                setGreenBg(btnWeekly);
+                tvCountUnit.setText("trong một tuần");
+                btnWatchMode = btnWeekly;
+                watchMode = 1;
+                break;
+            case R.id.g2_btn_monthly:
+                setWhiteBg(btnWatchMode);
+                setGreenBg(btnMonthly);
+                tvCountUnit.setText("trong một tháng");
+                btnWatchMode = btnMonthly;
+                watchMode = 2;
+                break;
+            case R.id.g2_btn_yearly:
+                setWhiteBg(btnWatchMode);
+                setGreenBg(btnYearly);
+                tvCountUnit.setText("trong một năm");
+                btnWatchMode = btnYearly;
+                watchMode = 3;
+                break;
+        }
+    }
+
+    @OnClick({R.id.ck_type_check, R.id.ck_type_count})
+    public void checkCountType(View v) {
+        if (v.getId() == R.id.ck_type_check) {
+            uncheck(imgTypeCount);
+            check(imgTypeCheck);
+            editCount.setEnabled(false);
+            typeCount = 0;
+        } else if (v.getId() == R.id.ck_type_count) {
+            uncheck(imgTypeCheck);
+            check(imgTypeCount);
+            editCount.setEnabled(true);
+            typeCount = 1;
+        }
+    }
+
+    @OnClick({R.id.btn_unit_set, R.id.btn_unit_no})
+    public void setCountUnit(View v) {
+
     }
 
     public void setGreenBg(View v) {
@@ -104,6 +139,14 @@ public class HabitActivity extends AppCompatActivity implements View.OnClickList
 
     public void setWhiteBg(View v) {
         v.setBackground(ContextCompat.getDrawable(this, android.R.color.transparent));
+    }
+
+    public void check(ImageView img) {
+        img.setImageResource(R.drawable.ck_checked);
+    }
+
+    public void uncheck(ImageView img) {
+        img.setImageResource(R.drawable.ck_unchecked);
     }
 
     public void showEmpty(View v) {
