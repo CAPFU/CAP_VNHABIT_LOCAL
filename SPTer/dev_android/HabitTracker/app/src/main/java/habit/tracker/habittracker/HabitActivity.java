a9446c473420273747f3e67e0dbd70ed07fdebb0
DatTVT1 <dattvt1@fsoft.com.vn>
2018-10-10 09:50:46 +0700

    add back

--- SPTer/dev_android/HabitTracker/app/src/main/java/habit/tracker/habittracker/HabitActivity.java
+++ SPTer/dev_android/HabitTracker/app/src/main/java/habit/tracker/habittracker/HabitActivity.java
@@ -2,12 +2,23 @@ package habit.tracker.habittracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HabitActivity extends AppCompatActivity {
+
+    View btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_habit);
+
+        btnBack = findViewById(R.id.btn_back);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                HabitActivity.this.finish();
+            }
+        });
     }
 }
