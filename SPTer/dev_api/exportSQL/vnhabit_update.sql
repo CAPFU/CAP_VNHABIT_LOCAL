ca0c3c73a85c0b115926eecfbd1c85279b082908
dat <tvtd995a3@gmail.com>
2018-10-25 04:16:53 +0700

    add export sql

---
+++ SPTer/dev_api/exportSQL/vnhabit_update.sql
@@ -0,0 +1,303 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 24, 2018 at 11:16 PM
+-- Server version: 10.1.35-MariaDB
+-- PHP Version: 7.2.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `vnhabit`
+--
+CREATE DATABASE IF NOT EXISTS `vnhabit` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `vnhabit`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `about`
+--
+
+CREATE TABLE `about` (
+  `app_name` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `app_description` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `app_fqa` text COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `achievement`
+--
+
+CREATE TABLE `achievement` (
+  `achievement_id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `achievement_name` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `achievement_description` text COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `achievement_details`
+--
+
+CREATE TABLE `achievement_details` (
+  `user_id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `achievement_id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `count` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `feedback_id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `star_num` int(11) NOT NULL,
+  `feedback_description` text COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `group`
+--
+
+CREATE TABLE `group` (
+  `group_id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `group_name` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `parent_id` varchar(36) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `group_icon` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `group_description` text COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `group`
+--
+
+INSERT INTO `group` (`group_id`, `group_name`, `parent_id`, `group_icon`, `group_description`) VALUES
+('1', 'Sức khỏe', NULL, '', ''),
+('2', 'Tài chính', NULL, '', ''),
+('3', 'Gia đình', NULL, '', ''),
+('4', 'Học tập', NULL, '', ''),
+('5', 'Mua sắm', NULL, '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `habit`
+--
+
+CREATE TABLE `habit` (
+  `habit_id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_id` varchar(36) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `group_id` varchar(36) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `monitor_id` varchar(36) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `habit_name` text COLLATE utf8mb4_unicode_ci,
+  `habit_target` tinyint(1) DEFAULT '0',
+  `habit_type` tinyint(1) DEFAULT '0',
+  `monitor_type` tinyint(1) DEFAULT '0',
+  `monitor_unit` text COLLATE utf8mb4_unicode_ci,
+  `monitor_number` int(11) DEFAULT NULL,
+  `start_date` date DEFAULT NULL,
+  `end_date` date DEFAULT NULL,
+  `created_date` date DEFAULT NULL,
+  `habit_color` text COLLATE utf8mb4_unicode_ci,
+  `habit_description` text COLLATE utf8mb4_unicode_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `monitor_date`
+--
+
+CREATE TABLE `monitor_date` (
+  `monitor_id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `habit_id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `mon` tinyint(1) DEFAULT '1',
+  `tue` tinyint(1) DEFAULT '1',
+  `wed` tinyint(1) DEFAULT '1',
+  `thu` tinyint(1) DEFAULT '1',
+  `fri` tinyint(1) DEFAULT '1',
+  `sat` tinyint(1) DEFAULT '1',
+  `sun` tinyint(1) DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reminder`
+--
+
+CREATE TABLE `reminder` (
+  `reminder_id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `habit_id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `reminder_time` text COLLATE utf8mb4_unicode_ci,
+  `repeat_time` int(11) DEFAULT NULL,
+  `reminder_description` text COLLATE utf8mb4_unicode_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tracking`
+--
+
+CREATE TABLE `tracking` (
+  `tracking_id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `habit_id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `current_date` text COLLATE utf8mb4_unicode_ci,
+  `count` int(11) DEFAULT '0',
+  `tracking_description` text COLLATE utf8mb4_unicode_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `username` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date_of_birth` date NOT NULL,
+  `gender` tinyint(1) NOT NULL DEFAULT '-1',
+  `user_icon` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `avatar` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_description` text COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `achievement`
+--
+ALTER TABLE `achievement`
+  ADD PRIMARY KEY (`achievement_id`);
+
+--
+-- Indexes for table `achievement_details`
+--
+ALTER TABLE `achievement_details`
+  ADD PRIMARY KEY (`user_id`,`achievement_id`),
+  ADD KEY `achievement_id` (`achievement_id`);
+
+--
+-- Indexes for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`feedback_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `group`
+--
+ALTER TABLE `group`
+  ADD PRIMARY KEY (`group_id`);
+
+--
+-- Indexes for table `habit`
+--
+ALTER TABLE `habit`
+  ADD PRIMARY KEY (`habit_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `group_id` (`group_id`),
+  ADD KEY `monitor_id` (`monitor_id`);
+
+--
+-- Indexes for table `monitor_date`
+--
+ALTER TABLE `monitor_date`
+  ADD PRIMARY KEY (`monitor_id`),
+  ADD KEY `habit_id` (`habit_id`);
+
+--
+-- Indexes for table `reminder`
+--
+ALTER TABLE `reminder`
+  ADD PRIMARY KEY (`reminder_id`),
+  ADD KEY `habit_id` (`habit_id`);
+
+--
+-- Indexes for table `tracking`
+--
+ALTER TABLE `tracking`
+  ADD PRIMARY KEY (`tracking_id`),
+  ADD KEY `habit_id` (`habit_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `achievement_details`
+--
+ALTER TABLE `achievement_details`
+  ADD CONSTRAINT `achievement_details_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `achievement_details_ibfk_2` FOREIGN KEY (`achievement_id`) REFERENCES `achievement` (`achievement_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD CONSTRAINT `feedback_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `habit`
+--
+ALTER TABLE `habit`
+  ADD CONSTRAINT `habit_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `habit_ibfk_2` FOREIGN KEY (`group_id`) REFERENCES `group` (`group_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `habit_ibfk_3` FOREIGN KEY (`monitor_id`) REFERENCES `monitor_date` (`monitor_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `monitor_date`
+--
+ALTER TABLE `monitor_date`
+  ADD CONSTRAINT `monitor_date_ibfk_1` FOREIGN KEY (`habit_id`) REFERENCES `habit` (`habit_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `reminder`
+--
+ALTER TABLE `reminder`
+  ADD CONSTRAINT `reminder_ibfk_1` FOREIGN KEY (`habit_id`) REFERENCES `habit` (`habit_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `tracking`
+--
+ALTER TABLE `tracking`
+  ADD CONSTRAINT `tracking_ibfk_1` FOREIGN KEY (`habit_id`) REFERENCES `habit` (`habit_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
